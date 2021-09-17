13c548c346954d5b9b5cf05270a2dd2c220c9bb5
Aryan Bhatt <aryanbhatt1002@gmail.com>
2021-09-17 16:47:43 +0530

    Ampherol added

--- js/custom.js
+++ js/custom.js
@@ -147,3 +147,13 @@ jQuery(document).ready(function($) {
 				// other options
 			});
 });
+(function($){
+	$(document).ready(function(){
+		$('ul.dropdown-menu [data-toggle=dropdown]').on('click', function(event) {
+			event.preventDefault();
+			event.stopPropagation();
+			$(this).parent().siblings().removeClass('open');
+			$(this).parent().toggleClass('open');
+		});
+	});
+})(jQuery);
